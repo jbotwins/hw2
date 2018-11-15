4ac4292c649fa5611b95f988452cc876a1146182
jbot <jbot@hellabicycles.com>
2018-11-14 20:44:11 -0800

    Added select option.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 let exchangeRates = [];
 //console.log(exchangeRates);
-function fetchCurrencyData() {
-  fetch('https://api.exchangeratesapi.io/latest')
+let baseURL = "https://api.exchangeratesapi.io/latest?base="
+let baseAcronym = "EUR";
+let fetchURL = baseURL + baseAcronym;
+//console.log(fetchURL);
+
+ function fetchCurrencyData() {
+//   console.log(baseAcronym);
+//   console.log(fetchURL);
+  fetch(fetchURL)
     .then(response => response.json())
     .then(fetchedData => {
       rawData = fetchedData;
       exchangesBase = fetchedData.base;
       exchangeRates = fetchedData.rates;
-      console.log(rawData);
+      //console.log(rawData);
       //console.log(exchangesBase);
       //console.log(exchangeRates);
       //console.log(typeof exchangeRates);
       //state.currencyData = data;
+      displaySelect();
       displayData();
     });
 }
+
+function displaySelect() {
+  let selectOutput = document.querySelector('#selectBase');
+  for (let key in exchangeRates) {
+    let item = exchangeRates[key];
+    //console.log(key);
+    let newSelect = document.createElement('option');
+    newSelect.textContent = key;
+    selectOutput.appendChild(newSelect);
+  }
+};
 
 function displayData() {
   let output = document.querySelector('#output');
@@ -25,11 +44,11 @@ function displayData() {
   for (let key in exchangeRates) {
     let item = exchangeRates[key];
     const baseValue = 1;
-    let propotionalRate = baseValue/item;
+    let propotionalRate = baseValue / item;
     if (propotionalRate > .5) {
       //console.log('item is: ' + item + '. ' + 'exchangeRates[key] is: ' + exchangeRates[key] + 'key is: ' + key);
       //Build proportion to Base Currency
-      let proportionalHeight = 'height:' + 100*propotionalRate + 'px';
+      let proportionalHeight = 'height:' + 100 * propotionalRate + 'px';
       //console.log(proportionalHeight);
       //console.log(propotionalRate);
       // Build column
@@ -56,3 +75,17 @@ function render() {
   fetchCurrencyData();
   displayData();
 };
+
+function rerender() {
+  let output = document.querySelector('#output');
+  output.innerHTML = '';
+  let input = document.querySelector('#selectBase');
+  //console.log(input);
+  let value = input.value;
+  //console.log(value);
+  baseAcronym = input.value;
+  console.log(baseAcronym);
+  fetchURL = baseURL + baseAcronym;
+  console.log(fetchURL);
+  fetchCurrencyData();
+}
